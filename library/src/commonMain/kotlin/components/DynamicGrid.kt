ade30568af625b9da74daf71a4a563b4d9bc2f18
iksanul15 <>
2025-09-09 14:57:01 +0700

    DynamicGrid: Init

---
+++ library/src/commonMain/kotlin/components/DynamicGrid.kt
@@ -0,0 +1,39 @@
+package com.impacto.impactoui.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun <T>DynamicGrid(
+    items: List<T>,
+    columns: Int = 2,
+    rowContent: @Composable (T) -> Unit
+) {
+    val rows = items.chunked(columns)
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(rows) { rowItems ->
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                rowItems.forEach { item ->
+                    rowContent(item)
+                }
+            }
+        }
+    }
+}
