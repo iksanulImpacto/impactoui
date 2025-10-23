d83684f3b9e1814a330f5a60e91d29300924ecfa
Ardi Naufan <ardi@impacto.id>
2025-10-23 14:18:46 +0700

    Fixing Dialog

--- library/src/commonMain/kotlin/dialog/BottomSheetDialog.kt
+++ library/src/commonMain/kotlin/dialog/BottomSheetDialog.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.impacto.impactoui.colors.AppColors
@@ -12,12 +14,16 @@ import com.impacto.impactoui.colors.AppColors
 @Composable
 fun BottomSheetDialog(
     onDismiss: () -> Unit,
-    content: @Composable () -> Unit
+    modifier: Modifier = Modifier,
+    sheetState: SheetState = rememberModalBottomSheetState(),
+    content: @Composable () -> Unit,
 ) {
     ModalBottomSheet(
         onDismissRequest = onDismiss,
-        modifier = Modifier.fillMaxWidth(),
-        containerColor = AppColors.White
+        modifier = modifier,
+        containerColor = AppColors.White,
+        sheetState = sheetState
+
     ) {
         content()
     }
