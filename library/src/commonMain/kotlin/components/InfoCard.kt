e381412df677c6a8f405390a4b2739094a70f333
iksanul15 <>
2025-12-01 13:54:56 +0700

    Add Parameter in InfoCard

--- library/src/commonMain/kotlin/components/InfoCard.kt
+++ library/src/commonMain/kotlin/components/InfoCard.kt
@@ -1,5 +1,6 @@
 package com.impacto.impactoui.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -9,6 +10,9 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.impacto.impactoui.colors.AppColors
 
@@ -16,19 +20,30 @@ import com.impacto.impactoui.colors.AppColors
 fun InfoCard(
     modifier: Modifier = Modifier,
     contentPadding: PaddingValues = PaddingValues(16.dp),
+    containerRadius: Dp = 12.dp,
+    elevationCard: Dp = 0.dp,
+    containerColor: Color = AppColors.White,
+    backgroundBrush: Brush? = null,
     content: @Composable () -> Unit
 ) {
     Card(
         modifier = modifier.fillMaxWidth(),
-        shape = RoundedCornerShape(12.dp),
+        shape = RoundedCornerShape(containerRadius),
         colors = CardDefaults.cardColors(
-            containerColor = AppColors.White
+            containerColor = if (backgroundBrush != null) Color.Transparent else containerColor
         ),
-        elevation = CardDefaults.cardElevation(defaultElevation = 0.dp)
+        elevation = CardDefaults.cardElevation(defaultElevation = elevationCard)
     ) {
         Box(
             modifier = Modifier
                 .fillMaxWidth()
+                .then(
+                    if (backgroundBrush != null) {
+                        Modifier.background(backgroundBrush)
+                    } else {
+                        Modifier
+                    }
+                )
                 .padding(contentPadding)
         ) {
             content()
