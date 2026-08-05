db9983113d2f1b5015f9cc2ea99f1d08e848f7f4
iksanul15 <iksanul@impacto.id>
2026-08-05 14:09:19 +0700

    Update Version 1.1.5, remove unused import in AppBasicTextField

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.impacto.impactoui"
-version = "1.1.4"
+version = "1.1.5"
 
 kotlin {
     // Target JVM (desktop / server)
@@ -112,7 +112,7 @@ mavenPublishing {
     coordinates(
         groupId = "io.github.iksanulimpacto",
         artifactId = "impactoui", // Nama pustaka Anda
-        version = "1.1.4"
+        version = "1.1.5"
     )
 
     pom {
--- library/src/commonMain/kotlin/forms/AppBasicTextField.kt
+++ library/src/commonMain/kotlin/forms/AppBasicTextField.kt
@@ -26,7 +26,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.text.TextStyle
